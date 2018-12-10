48c7bafc1a877f24be280cf7f395245885ab3628
Huck Kim <huckkim2001@gmail.com>
2018-12-10 06:23:38 +0000

    First Commit

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Extinctathon</h1>
+          <p>Monitored by MaddAddam. Adam named the living animals, MaddAddam names the dead ones.</p>
+        </div>
+      </div>
+    );
+  }
+}
