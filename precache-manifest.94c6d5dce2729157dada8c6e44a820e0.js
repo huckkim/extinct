220f4e929ef25d9a59f66efc511bf0b977c4bc30
kimh8399 <kimh8399@googleapps.wrdsb.ca>
2018-12-17 15:03:27 -0500

    Updates

---
+++ precache-manifest.94c6d5dce2729157dada8c6e44a820e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5d1a570c3bd51a129ee",
+    "url": "/extinct/static/js/runtime~main.d5d1a570.js"
+  },
+  {
+    "revision": "a6c6be05874fdcc7b81f",
+    "url": "/extinct/static/js/main.a6c6be05.chunk.js"
+  },
+  {
+    "revision": "7ba92b99affdddcc75e5",
+    "url": "/extinct/static/js/1.7ba92b99.chunk.js"
+  },
+  {
+    "revision": "a6c6be05874fdcc7b81f",
+    "url": "/extinct/static/css/main.862f2b1b.chunk.css"
+  },
+  {
+    "revision": "689adf34c712ed10f4166f13c011cd35",
+    "url": "/extinct/index.html"
+  }
+];
